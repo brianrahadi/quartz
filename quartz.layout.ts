1252386f3eaad59fade63af85f11213c1aa2cedd
Brian Rahadi <brian.rahadi@gmail.com>
2026-04-16 22:40:45 -0700

    fix: ponyo

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -10,12 +10,12 @@ const recentNotes = [
       f.slug!.startsWith("posts/") && f.slug! !== "posts/index" && !f.frontmatter?.noindex,
     linkToMore: "posts/" as SimpleSlug,
   }),
-  Component.RecentNotes({
-    title: "Recent Notes",
-    limit: 2,
-    filter: (f) => f.slug!.startsWith("notes/") && f.slug! !== "notes/index" && !f.frontmatter?.noindex,
-    linkToMore: "notes/" as SimpleSlug,
-  }),
+  // Component.RecentNotes({
+  //   title: "Recent Notes",
+  //   limit: 2,
+  //   filter: (f) => f.slug!.startsWith("notes/") && f.slug! !== "notes/index" && !f.frontmatter?.noindex,
+  //   linkToMore: "notes/" as SimpleSlug,
+  // }),
 ]
 
 // components shared across all pages
